e619a0fd5c043aea15bc3a23bc8ad614f43a07e6
Ziqin Gong <gongzq0301@gmail.com>
2024-04-15 13:03:00 +0800

    Pass api key from env var

--- mw-lookup/MWLookup.swift
+++ mw-lookup/MWLookup.swift
@@ -13,13 +13,22 @@ struct MWLookup: AsyncParsableCommand {
     @Argument(help: "The word to look up.")
     var word: String
 
-    @Option(name: [.customShort("k"), .long], help: "API key.")
-    var apiKey: String
+    @Option(name: [.customShort("k"), .long], help: "API key. Can also be passed by MW_API_KEY.")
+    var apiKey: String?
 
     mutating func run() async throws {
-        print("You want to look up `\(word)' via the API key \(apiKey).")
+        let validKey: String
 
-        let response = try await queryAndParse(word: word, apiKey: apiKey)
+        if let apiKey {
+            validKey = apiKey
+        } else if let key = ProcessInfo.processInfo.environment["MW_API_KEY"] {
+            validKey = key
+        } else {
+            fatalError("No API key provided!")
+        }
+        print("You want to look up `\(word)' via the API key \(validKey).")
+
+        let response = try await queryAndParse(word: word, apiKey: validKey)
         printResponse(response)
     }
 }
